7ee339a5814f76e19bbf70f66d3c21d44b9e86f5
Davy <davyd.dabahian@cheetahdigital.com>
2024-09-04 10:15:07 +0300

    lecture 05 objects arrays

---
+++ lecture_05_js_objects_arrays/scripts/main.js
@@ -0,0 +1,8 @@
+const numbers = [1,2,3,5,7,11,13,17];
+
+const strings = ['this', 'is', 'an', 'array', 'of', 'simple', 'strings'];
+
+const products = [
+  { name: 'Saw', price: 100 },
+  { name: 'Hammer', price: 50 }
+]

229c55efe1991d5a1732c3819fd0eb5d11451fef
Davy <davyd.dabahian@cheetahdigital.com>
2024-09-05 23:19:13 +0300

    summary 03

---
+++ summary_03_js_arrays_functions/scripts/main.js
@@ -0,0 +1,13 @@
+// Здесь будем писать код
+
+const numbers = [1,2,3,5,9,7,11,13,9,17,18,9,101,191];
+
+const strings = ['this', 'is', 'an', 'array', 'of', 'simple', 'strings'];
+
+const products = [
+  { name: 'Saw', price: 100, category: 'tools' },
+  { name: 'Hammer', price: 50, category: 'tools' },
+  { name: 'Ball', price: 20, category: 'sport' },
+  { name: 'Shoes', price: 150, category: 'sport' },
+  { name: 'Truck', price: 75, category: 'toys' }
+]

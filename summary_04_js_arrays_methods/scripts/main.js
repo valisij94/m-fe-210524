5277bc526789d7833c83b1fedeb0511d84235b1f
Davy <davyd.dabahian@cheetahdigital.com>
2024-09-13 09:24:04 +0300

    summary 04

---
+++ summary_04_js_arrays_methods/scripts/main.js
@@ -0,0 +1,20 @@
+// Здесь будем писать код
+
+const numbers = [1,2,3,5,9,7,11,13,9,17,18,9,101,191];
+
+const strings = ['this', 'is', 'an', 'array', 'of', 'simple', 'strings'];
+
+const weapons = ['sword', 'axe', 'naginata', 'crossbow', 'spear', 'bow', 'hammer', 'dagger', 'yari', 'protazan'];
+
+const archery = ['crossbow', 'bow'];
+const melee = ['sword', 'axe', 'naginata', 'spear', 'hammer', 'dagger', 'yari', 'protazan'];
+
+const dwarwes = [
+  { name: 'Torin', age: 232 },
+  { name: 'Dvalin', age: 301 },
+  { name: 'Balin', age: 454 },
+  { name: 'Filli', age: 198 },
+  { name: 'Killi', age: 203 }
+]
+
+const bilbo = { name: 'Bilbo', age: 34 };

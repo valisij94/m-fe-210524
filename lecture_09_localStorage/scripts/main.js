ea53f720bd4184d35547ab1c30c5bb8331dab1ee
Davy <davyd.dabahian@cheetahdigital.com>
2024-09-16 09:29:36 +0300

    lecture 09 localStorage

---
+++ lecture_09_localStorage/scripts/main.js
@@ -0,0 +1,13 @@
+const numbers = [1,2,3,5,9,7,11,13,9,17,18,9,101,191];
+
+const strings = ['this', 'is', 'an', 'array', 'of', 'simple', 'strings', 'Timon', 'Simba', 'Pumba', 'bravo', 'StarTrack'];
+
+const products = [
+  { name: 'Saw', price: 100, category: 'tools' },
+  { name: 'Hammer', price: 50, category: 'tools', discount: true },
+  { name: 'Ball', price: 20, category: 'sport' },
+  { name: 'Shoes', price: 150, category: 'sport', discount: true },
+  { name: 'Truck', price: 75, category: 'toys' },
+  { name: 'Drill', price: 125, category: 'tools', discount: true },
+  { name: 'JigSaw', price: 250, category: 'tools' },
+]

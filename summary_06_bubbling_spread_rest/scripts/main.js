e643cf691f5a6dad04a40d4ffd160be3db4694ae
Davy <davyd.dabahian@cheetahdigital.com>
2024-09-26 21:48:12 +0300

    summary 06 spread rest destructure

---
+++ summary_06_bubbling_spread_rest/scripts/main.js
@@ -0,0 +1,80 @@
+const numbers = [1, 2, 3, 4];
+
+const first = ['this', 'is', 'the', 'first', 'part'];
+const second = ['and', 'this', 'is', 'the', 'second', 'one'];
+const strings = ['another', 'array', 'of', 'simple', 'strings'];
+
+const dvalin = {
+  name: 'Dvalin',
+  age: 232,
+  weapon: 'axe',
+  skills: {
+    melee: 12
+  }
+}
+
+const balin = {
+  name: 'Balin',
+  age: 320,
+  weapon: 'sword',
+  skills: {
+    melee: 8
+  }
+}
+
+const filli = {
+  name: 'Filli',
+  age: 172,
+  weapon: 'spear',
+  skills: {
+    melee: 9
+  }
+}
+
+const killi = {
+  name: 'Killi',
+  age: 163,
+  weapon: 'crossbow',
+  skills: {
+    melee: 7,
+    archery: 10
+  }
+}
+
+const torin = {
+  name: 'Torin',
+  nickname: 'The Oakshield',
+  age: 201,
+  weapons: ['sword', 'hammer', 'dagger'],
+  skills: {
+    melee: 10,
+    archery: 5,
+    magic: 2
+  }
+}
+
+const nori = {
+  name: 'Nori',
+  age: 415,
+  weapons: ['hammer', 'dagger', 'bow', 'knives'],
+  skills: {
+    melee: 7,
+    archery: 3
+  }
+}
+
+const standardArmour = ['helmet', 'armour', 'shield'];
+
+const standardAmmo = {
+  bowler: 'steel bowler',
+  flask: 'metal flask',
+  flintstone: 'mechanical flintstone',
+  food: ['meat', 'nuts', 'bread', 'ale'],
+  others: {
+    socks: 'wool socks',
+    jacket: 'leather jacket'
+  }
+}
+
+// Для задачи 9 и далее
+const torinTroops = [];

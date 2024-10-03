e7c6fcb70e6c53925d5e933d8cc4c031df48bc1f
Davy <davyd.dabahian@cheetahdigital.com>
2024-10-03 23:38:42 +0300

    summary 07 async, DOM

---
+++ summary_07_async/scripts/main.js
@@ -0,0 +1,9 @@
+const products = [
+  { name: 'Saw', price: 100, category: 'tools' },
+  { name: 'Hammer', price: 50, category: 'tools', discount: true },
+  { name: 'Ball', price: 20, category: 'sport' },
+  { name: 'Shoes', price: 150, category: 'sport', discount: true },
+  { name: 'Truck', price: 75, category: 'toys' },
+  { name: 'Drill', price: 125, category: 'tools', discount: true },
+  { name: 'JigSaw', price: 250, category: 'tools' },
+]

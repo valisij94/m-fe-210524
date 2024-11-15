5818d190609aeb454863370afc654c36ec88ff41
Davy <davyd.dabahian@cheetahdigital.com>
2024-11-15 10:30:56 +0200

    summary 13 react basics

---
+++ summary_13_react_basics/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import Copyright from './components/copyright/Copyrgiht';
+
+function App() {
+
+  const middleEartAreas = ['Moria', 'Rivendalle', 'Isengard', 'Rohan', 'Gondor', 'Hobbiton', 'Shire'];
+  return (
+    <div className="App simpleStyle">
+    </div>
+  );
+}
+
+export default App;
---
+++ summary_13_react_basics/src/data/persons.js
@@ -0,0 +1,41 @@
+const Torin = {
+  name: 'Torin',
+  nickname: 'The Oakshield',
+  race: 'Dwarwes',
+  age: 137,
+  weapons: ['Sword', 'Axe', 'Spear']
+}
+
+const Gandalf = {
+  name: 'Gandalf',
+  race: 'Mayar',
+  nickname: 'The Grey',
+  age: 804,
+  weapons: ['Magic', 'Sword']
+}
+
+const Bilbo = {
+  name: 'Bilbo',
+  age: 56,
+  race: 'Hobbits',
+  weapons: ['Dagger']
+}
+
+const programe = ['Introduction', 'Melee', 'Archery tournament', 'Magic show', 'Final banquet'];
+
+const detailed = [
+  {
+    name: 'Radagast',
+    nickname: 'The Brown',
+    subject: 'The impact of Rostgobel\'s rabbits on the ecology of the Fangorn forest'
+  },
+  {
+    name: 'Saruman',
+    nickname: 'The White',
+    subject: 'Organizing video conferences using Palantir'
+  },
+  {
+    name: 'Elrond',
+    subject: 'generational conflict: parents and children'
+  }
+]

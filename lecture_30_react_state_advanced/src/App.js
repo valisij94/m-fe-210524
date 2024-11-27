49c76be549c8f6ce57edd2fec50988f2a380a930
Davy <davyd.dabahian@cheetahdigital.com>
2024-11-27 13:31:03 +0200

    lecture 30 results

--- lecture_30_react_state_advanced/src/App.js
+++ lecture_30_react_state_advanced/src/App.js
@@ -1,15 +1,28 @@
 import { useState } from 'react';
 import './App.css';
 
-import LoginForm from './components/loginForm/LoginForm';
+import TodoList from './components/todoList/TodoList';
+import TodoStats from './components/todoList/TodoStats';
+import LoginForm from './components/loginForm/LoginForm'
 import Greeting from './components/greeting/Greeting';
 
 function App() {
+
+  const [todos, setTodos] = useState(['Buy a car', 'Buy cryptocurrencies', 'Feed the dog']);
+
+  const dropTodo = () => {
+    setTodos( oldTodos => {
+      return oldTodos.slice(0, -1);
+    });
+  }
 
   const [loginValue, setLoginValue] = useState('');
 
   return (
     <div className="App simpleStyle">
+      <TodoList todos={todos} dropTodo={dropTodo}/>
+      <TodoStats todos={todos}/>
+
       <LoginForm setLoginValue={setLoginValue}/>
       <Greeting userName={loginValue} />
     </div>

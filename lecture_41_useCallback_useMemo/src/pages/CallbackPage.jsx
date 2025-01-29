881b4ea0536da3d4f1345c1cc552254a63187cfb
Davy <davyd.dabahian@cheetahdigital.com>
2025-01-29 14:30:22 +0200

    lecture 41 results

--- lecture_41_useCallback_useMemo/src/pages/CallbackPage.jsx
+++ lecture_41_useCallback_useMemo/src/pages/CallbackPage.jsx
@@ -1,13 +1,16 @@
-import { useState } from "react"
+import { memo, useCallback, useMemo, useState } from "react"
 
-function AttemptsList( { attempts} ) {
+function AttemptsList( { attempts, dropAttempt } ) {
 
+  console.log('Render attepts list');
   return (
     attempts && <div>
-      {attempts.map( attempt => <p key={attempt.key}>Attempt time: {attempt.value}</p>) }
+      {attempts.map( attempt => <p onClick={() => {dropAttempt(attempt.key)}} key={attempt.key}>Attempt time: {attempt.value}</p>) }
     </div>
   )
 };
+
+const MemoizedAttemptsList = memo(AttemptsList);
 
 export default function CallbackPage() {
 
@@ -19,19 +22,27 @@ export default function CallbackPage() {
     setAttempts( old => ([...old, { key: stringifiedDate, value: input }]));
   }
 
-  const heavyCalculation = () => {
-    //console.log('Called HEAVY FUNCTION');
+  const dropAttempt = useCallback((attemptId) => {
+    setAttempts( old => old.filter( el => el.key !== attemptId ) );
+  }, []);
+
+  const heavyCalculation = useMemo(() => {
+    console.log('Called HEAVY FUNCTION');
     // Do some heavy calculations...
     return 1;
-  }
+  }, []);
+
+  const memoizedAttemps = useMemo( () => {
+    return attempts.filter( el => el.value.indexOf('A') >= 0 );
+  }, [attempts])
 
   return (
     <div>
       <h3>Work with useCallback hook</h3>
       <input value={input} onChange={(e)=>setInput(e.target.value)}/>
       <button onClick={addAttempt}>Add attempt</button>
-      <AttemptsList attempts={attempts} />
-      <p>Heavy calculations result: {heavyCalculation()}</p>
+      <MemoizedAttemptsList attempts={memoizedAttemps} dropAttempt={dropAttempt}/>
+      <p>Heavy calculations result: {heavyCalculation}</p>
     </div>
   )
 }

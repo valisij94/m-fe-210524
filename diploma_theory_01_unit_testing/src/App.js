17ee783e128a84339fef9950f665d7657d9d4f9d
Davy <davyd.dabahian@cheetahdigital.com>
2025-02-05 09:15:21 +0200

    diploma theory 01 unit testing

---
+++ diploma_theory_01_unit_testing/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import './App.css';
+
+import CommonPage from './components/callbacksExample/CommonPage.jsx';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h2>Jest testing framework - introduction</h2>
+    </div>
+  );
+}
+
+export default App;

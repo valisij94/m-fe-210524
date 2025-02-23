f67b9533da2df4ba37aa78c10e30269b65676084
Davy <davyd.dabahian@cheetahdigital.com>
2025-02-23 23:13:32 +0200

    diploma theory 04 results

--- diploma_theory_04_localStorage/src/App.js
+++ diploma_theory_04_localStorage/src/App.js
@@ -5,21 +5,41 @@ import Copyright from './components/copyright/Copyrgiht.jsx';
 import Home from './pages/Home.jsx';
 import ProductsPage from './pages/ProductsPage.jsx';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
 
   const [counter, setCounter] = useState(0);
 
-  const [inputs, setInputs] = useState({name: '', email: '', phone: ''});
+  const [inputs, setInputs] = useState({login: '', email: '', phone: ''});
   const handleInputChange = (e) => {
     const { name } = e.target;
-    setInputs( old => ({...old, [name]: e.target.value}));
+    const newState = { ...inputs, [name]: e.target.value };
+    setInputs( newState );
+    localStorage.setItem('inputs', JSON.stringify(newState));
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log('Form submitted!')
   }
+
+  const updateCounter = (newValue) => {
+    localStorage.setItem('counter', newValue);
+    setCounter(newValue);
+  }
+
+  useEffect( () => {
+    const persistedCounter = localStorage.getItem('counter');
+    if (persistedCounter) {
+      setCounter(+persistedCounter);
+    }
+
+    const persistedInputs = localStorage.getItem('inputs');
+    if (persistedInputs) {
+      setInputs(JSON.parse(persistedInputs));
+    }
+  }, []);
 
   return (
     <div className="App">
@@ -35,12 +55,12 @@ function App() {
         </Routes>
       </div>
       <p>Counter value: {counter}</p>
-      <button onClick={() => setCounter(counter + 1)}>Increment</button>
-      <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+      <button onClick={() => { updateCounter(counter + 1); }}>Increment</button>
+      <button onClick={() => { updateCounter(counter - 1); }}>Decrement</button>
 
       <h3>Simple form</h3>
       <form>
-        <input type='text' value={inputs.name} name='login' placeholder='login' onChange={handleInputChange}></input>
+        <input type='text' value={inputs.login} name='login' placeholder='login' onChange={handleInputChange}></input>
         <input type='text' value={inputs.email} name='email' placeholder='email' onChange={handleInputChange}></input>
         <input type='text' value={inputs.phone} name='phone' placeholder='phone' onChange={handleInputChange}></input>
         <button onClick={handleSubmit}>Submit</button>

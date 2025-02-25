052d61b87c00bd1f659aa73313e5cb0432c6cb4b
Davy <davyd.dabahian@cheetahdigital.com>
2025-02-25 23:35:07 +0200

    diploma theory 05 MUI

---
+++ diploma_theory_05_react-mui/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import SimpleComponent from './components/mui-components/SimpleComponent.jsx';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h2>React MUI Intro</h2>
+      <SimpleComponent />
+    </div>
+  );
+}
+
+export default App;
